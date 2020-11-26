d830d1343d017f2e40fd5185fc657548745e2067
Smriti111 <smritikpriya@gmail.com>
2020-11-26 07:33:07 +0530

    shortened the url using bitly-api

--- tiny_url.py
+++ tiny_url.py
@@ -1,39 +1,51 @@
 import requests
-username='pssmriti'
-password='Smriti@123'
+username = 'pssmriti'
+password = 'Smriti@123'
 
-#get access token
-auth_res=requests.post("https://api-ssl.bitly.com/oauth/access_token", auth=(username, password)))
+# get access token
+auth_res = requests.post(
+    "https://api-ssl.bitly.com/oauth/access_token", auth=(username, password))
 
 #if status is ok
-if auth_res.status_code==200:
-    access_token=auth_res.content.decode()
+if auth_res.status_code == 200:
+    access_token = auth_res.content.decode()
     print("[!] Recieved the access token", access_token)
 else:
     print("[!] Error while getting the access token")
     exit()
+#access_token="ef98d23dde13dd5938f9918295978c42cb09f2ed"
 
 
-
-#construct the request header with authorization
+# construct the request header with authorization
 headers = {"Authorization": f"Bearer {access_token}"}
 
-#get the Group UID associated with your bitly account
+# get the Group UID associated with your bitly account
 
-group_res=requests.get("https://api-ssl.bitly.com/v4/groups", headers=headers)
+group_res = requests.get(
+    "https://api-ssl.bitly.com/v4/groups", headers=headers)
 
-if group_res.status_code==200:
-    groups_data=group_res.json()['groups'][0]
-    guid=groups_data['guid']
+if group_res.status_code == 200:
+    groups_data = group_res.json()['groups'][0]
+    guid = groups_data['guid']
 
 else:
     print("[!] cannot fetch the GUID exiting ....")
     exit()
 
 
-#url u wanna shorten
+# url u wanna shorten
 url = "https://www.thepythoncode.com/topic/using-apis-in-python"
 
-#post request to get the shortened url from api
+# post request to get the shortened url from api
 
-shorten_res=requests.post()
+shorten_res = requests.post("https://api-ssl.bitly.com/v4/shorten",json={"group_guid": guid, "long_url": url}, headers=headers)
+if shorten_res.status_code == 200:
+    link = shorten_res.json().get("link")
+    print("shortened link", link)
+
+qr_code_response=requests.get("https://api-ssl.bitly.com/v4/bitlinks/link/qr",headers=headers)
+if qr_code_response.status_code==200:
+    qr_code=qr_code_response.json().get("qr_code")
+    print(qr_code)
+else:
+    print(qr_code_response.status_code)
